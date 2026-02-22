ba3bf618fca3160f8c4620ea97b6aa9fd2bb2f0d
Hammam GNF <hammamgonjil@gmail.com>
2026-02-23 04:27:48 +0700

    feat: implement production-ready marketplace database schema

---
+++ app/Models/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $table = 'orders';
+    protected $fillable = [
+        'user_id',
+        'order_number',
+        'sub_total',
+        'grand_total',
+        'status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
